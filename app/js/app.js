93583f3491c2a123855e5c8b2db6a0889e1d9c05
Artem Netesov <artem@ASUS-PC.localdomain>
2020-08-28 16:36:50 +0700

    Сверстал Отзывы

--- app/js/app.js
+++ app/js/app.js
@@ -1,16 +1,49 @@
+
 document.addEventListener("DOMContentLoaded", function() {
 
 //Swiper
 
-  // const swiperSpec = new Swiper('.swiper-container', {
-  //   loop: true,
+  const swiperGas = new Swiper('#gazel', {
+    loop: true,
+    slidesPerView: 1
 
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     clickable: true,
-  //   },
-  // });
+    // pagination: {
+      // el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+  });
 
+  const swiperHeel = new Swiper('#heel', {
+    loop: true,
+    slidesPerView: 1
+  });
+
+  const swiperTruck = new Swiper ('#truck', {
+    loop: true,
+    slidesPerView: 1
+  })
+
+  const tabLink = document.querySelectorAll('.tabs__item')
+
+  if(tabLink.length) {
+    tabLink.forEach(i => {
+      i.addEventListener('click', event => {
+        tabLink.forEach( t => t.classList.remove('active'))
+        i.classList.add('active')
+      })
+    })
+  }
+
+  const textRew = document.querySelectorAll('._text-review')
+
+  textRew.forEach( i => {
+    const t = textCrop(i, 256);
+    i.innerHTML = t
+  })
+
+  function textCrop(text, length) {
+    return text.innerHTML.trim().slice(0, length).concat('...')
+  }
 
 
 });
