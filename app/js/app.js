f5b480dab8709f9a017b50aeb85cbbf05ea0dff4
Artem Netesov <artem@ASUS-PC.localdomain>
2020-09-01 14:17:10 +0700

    Сделал отображение скрытой части комментариев

--- app/js/app.js
+++ app/js/app.js
@@ -23,27 +23,78 @@ document.addEventListener("DOMContentLoaded", function() {
     slidesPerView: 1
   })
 
+//tabs
+
   const tabLink = document.querySelectorAll('.tabs__item')
 
   if(tabLink.length) {
+    document.querySelector(tabLink[0].getAttribute('href')).style.display = 'block'
+    swiperGas.update()
     tabLink.forEach(i => {
       i.addEventListener('click', event => {
-        tabLink.forEach( t => t.classList.remove('active'))
+        event.preventDefault()
+        tabLink.forEach( t => {
+          t.classList.remove('active')
+          document.querySelector(t.getAttribute('href')).style.display = 'none'
+        })
+
         i.classList.add('active')
+        const tabContent = document.querySelector(i.getAttribute('href'))
+        tabContent.style.display = 'block'
+        
+        swiperGas.update()
+        swiperHeel.update()
+        swiperTruck.update()
       })
     })
   }
 
-  const textRew = document.querySelectorAll('._text-review')
+  //text crop/show
 
-  textRew.forEach( i => {
-    const t = textCrop(i, 256);
-    i.innerHTML = t
+  const reviews = document.querySelector('.reviews')
+  const textReview = reviews.querySelectorAll('._text-review')
+  let textNotCrop = []
+  const length = 256
+
+  textReview.forEach( item => {
+    const text = item.innerHTML
+    textNotCrop.push(text)
+    item.innerHTML = textCrop(text, length)
   })
 
+  if(reviews) {
+    reviews.addEventListener('click', event => {
+    event.preventDefault()
+    const elLink = event.target
+    const elCurText = elLink.previousElementSibling
+    const curText = elCurText.innerHTML
+
+    if(elLink.dataset.more === 'read') {
+      textShow(elCurText)
+      elLink.innerHTML = 'Скрыть'
+      elLink.dataset.more = 'hide'
+    }else if(elLink.dataset.more === 'hide') {
+      elCurText.innerHTML = textCrop(curText, length)
+      elLink.innerHTML = 'Читать полностью'
+      elLink.dataset.more = 'read'
+
+    }
+  })}
+
+
   function textCrop(text, length) {
-    return text.innerHTML.trim().slice(0, length).concat('...')
+    let result = text
+
+    if(text.length > length) {
+      result = text.substring(0, length-3) + '...'
+    }
+    return result
   }
 
+  function textShow(elText) {
+      const text = elText.innerHTML
+      const index = textNotCrop.findIndex( t => t.includes(text.substring(0, text.length - 3)))
+      elText.innerHTML = textNotCrop[index]
+  }
 
 });
