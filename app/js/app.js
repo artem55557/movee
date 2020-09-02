3306d4cd95eec0104c98c1e70562f8c6204765c8
Artem Netesov <artem@ASUS-PC.localdomain>
2020-09-02 17:33:58 +0700

    Сделал адаптив под мобильные устройства

--- app/js/app.js
+++ app/js/app.js
@@ -1,21 +1,37 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
+  const burger = document.querySelector('.burger-btn')
+  const menu = document.querySelector('.menu')
+
+  if(burger) {
+    burger.addEventListener('click', event => {
+      event.preventDefault()
+      burger.classList.toggle('open')
+      menu.classList.toggle('menu-open')
+
+      if(menu.classList.value.includes('menu-open')) {
+        document.body.style.overflow = 'hidden'
+      } else {
+        document.body.style.overflow = ''
+      }
+    })
+  }
+
 //Swiper
 
   const swiperGas = new Swiper('#gazel', {
     loop: true,
-    // slidesPerView: 1,
 
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
 
-    // pagination: {
-      // el: '.swiper-pagination',
-    //   clickable: true,
-    // },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
   });
 
   const swiperHeel = new Swiper('#heel', {
@@ -24,7 +40,11 @@ document.addEventListener("DOMContentLoaded", function() {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    }
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
   });
 
   const swiperTruck = new Swiper ('#truck', {
@@ -33,7 +53,11 @@ document.addEventListener("DOMContentLoaded", function() {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
-    }
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
   })
 
 //tabs
@@ -110,4 +134,75 @@ document.addEventListener("DOMContentLoaded", function() {
       elText.innerHTML = textNotCrop[index]
   }
 
+
+  //scroll by anchor
+
+  const anchors = document.querySelectorAll('.menu__link')
+  let animationTime = 300;
+  let framesCount = 20;
+  let topOffset = 55;
+
+  anchors.forEach(function(item) {
+    item.addEventListener('click', function(e) {
+      e.preventDefault();
+      menu.classList.remove('menu-open')
+      burger.classList.remove('open')
+      document.body.style.overflow = ''
+      scrollToAnchor(item);
+    });
+  });
+
+  function scrollToAnchor(item) {
+   
+    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset - topOffset;
+
+    let scroller = setInterval(function() {
+      let scrollBy = coordY / framesCount;
+
+      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+        window.scrollBy(0, scrollBy);
+      } else {
+        window.scrollTo(0, coordY);
+        clearInterval(scroller);
+      }
+    }, animationTime / framesCount);
+  }
+
+//active menu link
+
+  window.addEventListener('scroll', () => {
+    const header = document.querySelector('.firstscreen__header')
+    if (window.pageYOffset >= 100) {
+      header.style.height = '50px'
+    } else {
+      header.style = ''
+    }
+    activeLinkOnScroll()
+  })
+
+  function activeLinkOnScroll() {
+    for (let i = 0; i < anchors.length; i++) {
+      const link = anchors[i];
+      const item = document.querySelector(link.getAttribute('href'));
+      const itemHeight = document.querySelector(link.getAttribute('href')).getBoundingClientRect().height;
+      const itemOffset = offset(item).top;
+
+      let itemPoint = window.innerHeight * 0.3;
+
+      if ((pageYOffset > itemOffset - itemPoint) && pageYOffset < (itemOffset + itemHeight - itemPoint)) {
+        link.parentElement.classList.add('active')
+      } else {
+        link.parentElement.classList.remove('active')
+      }
+    }
+  }
+
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+  }
+
 });
+
